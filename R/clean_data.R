2681fe75ecbb928b18d0504b2124261c4da81676
AmeliePelletier <pela1504@usherbrooke.ca>
2023-04-20 15:40:12 -0800

    modification ben

---
+++ R/clean_data.R
@@ -0,0 +1,11 @@
+clean_data_cours = function(data){
+  data_list = lapply(data, function(x) read.table(x, sep=';'))
+  cours1 = data_list[[1]]
+  etud1 = data_list[[2]]
+  
+}
+
+
+clean_data_collab = function{
+  blablalba
+}
