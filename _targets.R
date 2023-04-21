fa421347c1a612632167f7fc8a0237029de877b6
MagalieBosse <magalie.bosse@usherbrooke.ca>
2023-04-21 17:49:39 -0400

    fonction nettoyage fonctionnelle

--- _targets.R
+++ _targets.R
@@ -2,22 +2,24 @@
 
 #Dependances#
 library(targets)
-tar_option_set(packages=c("MASS","igraph"))
+tar_option_set(packages=c("targets","dplyr", "rmarkdown", "tidyverse", "RSQLite", "igraph","stringr"))
 
 ##Pipeline##
 
 ##Packages
-list_packages <- c("targets","dplyr", "rmarkdown", "tidyverse", "RSQLite", "igraph")
-tar_target(
-  packages,
-  command = {
-    library(targets)
-    install.packages(list_packages)
-  }
-)
+#tar_target(
+#  packages,
+#  command = {
+#    install.packages(list_packages)
+#  }
+#)
 
 #Nettoyage#
 source("R/Nettoyage.R")
+source("R/basedonnees.R")
+source("R/analyse.R")
+source("R/Figures.R")
+
 list(
   tar_target(
     name = path,
@@ -34,92 +36,67 @@ list(
     name = Nettoyage, #fonction qui nettoye toutes les bases de donnees et les fusionnent
     command = clean_data(data)
   ),
-  tar_target(
-    name = Nettoyage,
-    command = clean_uniforme(data)
-  ),
-  tar_target(
-    name = Nettoyage,
-    command = clean_data_cours(data)
-  ),
-  tar_target(
-    name = Nettoyage,
-    command = clean_data_etudiant(data)
-  ),
-  tar_target(
-    name = Nettoyage,
-    command = clean_data_collab(data)
-  )
-)  
-
 ##Creation sql##
-source("R/basedonnees.R")
-
-list(
   tar_target(
-    name = basedonnees,
-    command = create_con(Nettoyage)
-  ),
-  tar_target(
-    name = basedonnees,
+    name = etudiant_sql,
     command = create_etudiant(Nettoyage)
-  ),
-  tar_target(
-    name = basedonnees,
-    command = create_cours(Nettoyage)
-  ),
-  tar_target(
-    name = basedonnees,
-    command = create_collab(Nettoyage)
   )
-)  
+#  tar_target(
+#    name = etudiant_sql,
+#    command = dbConnect(SQLite(),dbname="./data.db")
+#  ),
+#  tar_target(
+#    name = basedonnees3,
+#    command = create_cours(basedonnees1)
+#  ),
+#  tar_target(
+#    name = basedonnees4,
+#    command = create_collab(basedonnees1)
+#  ),
+
 ##Requetes sql##
-source("R/analyse.R")
- 
-list( 
-  tar_target(
-    name = analyse,
-    command = requete1(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete2(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete3(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete4(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete5(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete6(basedonnees)
-  ),
-  tar_target(
-    name = analyse,
-    command = requete7(basedonnees)
-  )
-) 
+
+#  tar_target(
+#    name = analyse,
+#    command = requete1(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete2(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete3(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete4(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete5(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete6(basedonnees)
+#  ),
+#  tar_target(
+#    name = analyse,
+#    command = requete7(basedonnees)
+#  ),
 
 ##Figures##
-source("R/Figures.R")
-list(
-  tar_target(
-    name=Figures,
-    command = fig_relation(analyse)
-  ),
-  tar_target(
-    name=Figures,
-    command = fig_sigle(analyse)
-  ),
-  tar_target(
-    name=Figures,
-    command = fig_session(analyse)
-  )
+
+#  tar_target(
+#    name=Figures,
+#    command = fig_relation(analyse)
+#  ),
+#  tar_target(
+#    name=Figures,
+#    command = fig_sigle(analyse)
+#  ),
+#  tar_target(
+#    name=Figures,
+#    command = fig_session(analyse)
+#  )
 )
