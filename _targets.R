3f69b11002e7368fbefa9ce78ebac9ba406cb5a9
AmeliePelletier <pela1504@usherbrooke.ca>
2023-04-20 20:30:40 -0800

    enlever source clean data

--- _targets.R
+++ _targets.R
@@ -8,10 +8,20 @@ tar_option_set(packages=c("MASS","igraph"))
 #source("R/Nettoyage.R")
 #source("R/basedonnees.R")
 #source("R/analyse.R")
-source("R/clean_data.R")
+
 
 ##Pipeline##
 #Nettoyage#
+list_packages <- c("dplyr", "rmarkdown", "tidyverse", "RSQLite", "igraph")
+
+tar_target(
+  packages,
+  command = {
+    library(targets)
+    install.packages(list_packages)
+  }
+)
+
 list(
   tar_target(
     name = path, # Cible
@@ -26,7 +36,7 @@ list(
   
   tar_target(
     name = data_cleaned,
-    command = clean_data(data)
+    command = Nettoyage(data)
   )
-
 )
+  
