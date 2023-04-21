8215c114f38aed201acfbfa2eaf48a04b1ec94a6
MagalieBosse <magalie.bosse@usherbrooke.ca>
2023-04-20 20:29:00 -0400

    new babe

--- _targets.R
+++ _targets.R
@@ -2,6 +2,11 @@
 
 #Dependances#
 library(targets)
+library(dplyr)
+library(rmarkdown)
+library(tidyverse)
+library(RSQLite)
+library(igraph)
 tar_option_set(packages=c("MASS","igraph"))
 
 #Scripts R reference#
