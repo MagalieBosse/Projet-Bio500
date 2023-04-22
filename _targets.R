7813633b8cad5682c8ccd8345b692cc030704857
MagalieBosse <magalie.bosse@usherbrooke.ca>
2023-04-21 23:03:49 -0400

    list complete a ajuster

--- _targets.R
+++ _targets.R
@@ -4,22 +4,15 @@
 library(targets)
 tar_option_set(packages=c("targets","dplyr", "rmarkdown", "tidyverse", "RSQLite", "igraph","stringr"))
 
-##Pipeline##
-
-##Packages
-#tar_target(
-#  packages,
-#  command = {
-#    install.packages(list_packages)
-#  }
-#)
+#Pipeline#
 
 #Nettoyage#
 source("R/nettoyage_v2.R")
-#source("R/basedonnees.R")
-#source("R/analyse.R")
-#source("R/Figures.R")
+source("R/basedonnees.R")
+source("R/analyse.R")
+source("R/Figures.R")
 
+#definir source donnees (fichiers excels)
 list(
   tar_target(
     name = path,
@@ -27,26 +20,33 @@ list(
     format = "file"
   ),
   
+#base de donnees
   tar_target(
     name = data, # Cible pour l'objet de donnees
     command = list.files(path, full.names = TRUE) # Lecture des donnees
   ),
+
+#nettoyer les donnees  
   tar_target(
     name = nettoyage, #fonction qui nettoye toutes les bases de donnees et les fusionnent
     command = clean_data(data)
-  )
+  ),
+
+#creation sql#
+  tar_target(
+   name = table_sql, #fonction qui cree toutes les tables sql
+   command = create_sql(nettoyage)
+    ),
+
+#creation des requetes sql
+    tar_target(
+      name = requete_donnees, #fonction qui utilise ces tables pour faire des analyses/requetes
+      command = requetes(table_sql)
+    ),
+
+#figures
+    tar_target(
+      name = figures_final, #fonction qui cree et enregistre toutes nos figures
+      command = fig_final(requete_donnees)
+    )
 )
-  ##Creation sql##
-#  tar_target(
-#   name = table_sql,
-#   command = create_sql(nettoyage)
-#    ),
-  #  tar_target(
-  #    name = requete_donnees,
-  #    command = requetes(table_sql)
-  #  ),
-  #  tar_target(
-  #    name = figures_final,
-  #    command = fig_final(requete_donnees)
-  #  )
-#)
