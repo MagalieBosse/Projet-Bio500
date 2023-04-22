9eb3e8a5867ca0382653b29344bce3b450242bce
AmeliePelletier <pela1504@usherbrooke.ca>
2023-04-21 18:06:05 -0800

    k

--- R/analyse.R
+++ R/analyse.R
@@ -1,22 +1,22 @@
-#requete 1 nombre de liens par etudiant
+requete1 = function(basedonnees){
 sql_requete1<-"
 SELECT etudiant1, count(etudiant2) AS nb_collaborations
 FROM collaboration_sql
 GROUP BY etudiant1
 ORDER BY nb_collaborations DESC;"
 nb_collab<-dbGetQuery(con,sql_requete1)
-head(nb_collab)
+}
 
-#requete 2 decompte de liens par paire d'etudiants
+requete2 = function(basedonnees){
 sql_requete2<-"
 SELECT etudiant1, etudiant2, COUNT (sigle) AS nb_liens 
 FROM collaboration_sql
 GROUP BY etudiant1, etudiant2
 ORDER BY nb_liens DESC;"
 nb_lienetudiant<-dbGetQuery(con,sql_requete2)
-head(nb_lienetudiant)
+}
 
-#requete 3 cours ayant le plus de collab
+requete3 = function(basedonnees){
 sql_requete3<-"
 SELECT sigle, count(DISTINCT etudiant1) AS nb_etudiant
 FROM collaboration_sql
@@ -24,37 +24,41 @@ INNER JOIN cours_sql USING (sigle)
 GROUP BY sigle
 ORDER BY nb_etudiant DESC;"
 resume_sigle<-dbGetQuery(con,sql_requete3)
-head(resume_sigle)
+}
 
-#requete 4 nombre etudiant par programme
+requete4 = function(basedonnees){
 sql_requete4<-"
 SELECT programme, count(prenom_nom) AS nb_par_prog
 FROM etudiant_sql
 GROUP BY programme;"
 etudiantprog<-dbGetQuery(con,sql_requete4)
+}
 
-#requete 5 nombre de collaboration par session
+requete5 = function(basedonnees){
 sql_requete5<-"
 SELECT session, COUNT(*) AS nb_collab_session
 FROM collaboration_sql
 GROUP BY session;"
 collab_session<-dbGetQuery(con,sql_requete5)
+}
 
-#requete 6 nb etudiants en tout
+requete6 = function(basedonnees){
 sql_requete6<-"
 SELECT COUNT (*) AS nb_etudianttotal
 FROM etudiant_sql"
 et_total<-dbGetQuery(con, sql_requete6)
 #sauver le nombre etudiant total
 nb_etudiant<-et_total$nb_etudianttotal
+}
 
-#requete 7 nb collaboration
+requete7 = function(basedonnees){
 sql_requete7<-"
 SELECT COUNT (*) AS nb_collabtotal
 FROM collaboration_sql"
 collab_total<-dbGetQuery(con, sql_requete7)
 #sauver le nombre de lignes de collab
 nb_collaboration<-collab_total$nb_collabtotal
+<<<<<<< HEAD
 
 #figures
 noms<-unique(etudiant_bon$prenom_nom)
@@ -92,3 +96,6 @@ mtext("Nom de la session",side=1,line=3,padj=2)
 #Tableau : faire dans markdown 
 print(etudiantprog)
 .
+=======
+}
+>>>>>>> f365292cdc7cd42c9089d762b07ce914b3559cd0
